ed877c59be3621729e32bd52c45d30257e19f277
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2024-10-13 13:38:52 +0700

    Step 8

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -4,12 +4,28 @@ import { validate } from "../middlewares/validate.js"
 import { RestaurantSchema, type Restaurant } from "../schema/restaurants.js"
 import { initializeRedisClient } from "../utils/client.js"
 import { nanoid } from "nanoid"
-import { cuisineKey, cuisinesKey, restaurantCuisinesKeyById, restaurantKeyById, reviewDetailsKeyById, reviewKeyById } from "../utils/keys.js"
+import { cuisineKey, cuisinesKey, restaurantCuisinesKeyById, restaurantKeyById, restaurantsByRatingKey, reviewDetailsKeyById, reviewKeyById } from "../utils/keys.js"
 import { errorResponse, successResponse } from "../utils/responses.js"
 import { checkRestaurantExists } from "../middlewares/checkRestaurantId.js"
 import { ReviewSchema, type Review } from "../schema/review.js"
 
 const routes = express.Router()
+
+routes.get("/", async ( req, res, next) => {
+    const { page = 1, limit = 10 } = req.query
+    const start = (Number(page) - 1 ) * Number(limit)
+    const end = start + Number(limit)
+
+    try {
+        const client = await initializeRedisClient()
+        const restaurantIds = await client.zRange(restaurantsByRatingKey, start, end, { REV: true})
+        const restaurants = await Promise.all(restaurantIds.map(id => client.hGetAll(restaurantKeyById(id))))
+
+        successResponse({ res: res, data: restaurants })
+    } catch (error) {
+        next(error)
+    }
+})
 
 routes.post("/", validate(RestaurantSchema),  async (req, res, next) => {
     const data = req.body as Restaurant
@@ -26,7 +42,11 @@ routes.post("/", validate(RestaurantSchema),  async (req, res, next) => {
                 client.sAdd(cuisineKey(cuisine), id),
                 client.sAdd(restaurantCuisinesKeyById(id), cuisine)
             ])),
-            client.hSet(restaurantKey, hashData)
+            client.hSet(restaurantKey, hashData),
+            client.zAdd(restaurantsByRatingKey, {
+                score: 0,
+                value: id,
+            })
         ])
 
         console.log(`Added ${addResult} field`)
@@ -65,10 +85,21 @@ routes.post("/:restaurantId/reviews", [checkRestaurantExists, validate(ReviewSch
         const reviewKey = reviewKeyById(restaurantId)
         const reviewDetailsKey = reviewDetailsKeyById(reviewId)
         const reviewData = { id: reviewId, ...data, timestamp: Date.now(), restaurantId }
+        const restaurantKey = restaurantKeyById(restaurantId)
 
-        await Promise.all([
+        const [reviewCount, setResult, totalStars] = await Promise.all([
             client.lPush(reviewKey, reviewId),
-            client.hSet(reviewDetailsKey, reviewData)
+            client.hSet(reviewDetailsKey, reviewData),
+            client.hIncrByFloat(restaurantKey, "totalStars", data.rating)
+        ])
+
+        const averageRating = Number((totalStars / reviewCount).toFixed(1))
+        await Promise.all([
+            client.zAdd(restaurantsByRatingKey, {
+                score: averageRating,
+                value: restaurantId,
+            }),
+            client.hSet(restaurantKey, "avgStars", averageRating),
         ])
 
         successResponse({ res: res, data: reviewData, message: "Review added"})
--- utils/keys.ts
+++ utils/keys.ts
@@ -9,3 +9,4 @@ export const reviewDetailsKeyById = (id: string) => getKeyName("review_details",
 export const cuisinesKey = getKeyName("cuisines")
 export const cuisineKey = (name: string) => getKeyName("cuisine", name)
 export const restaurantCuisinesKeyById = (id: string) => getKeyName("restaurant_cuisines", id)
+export const restaurantsByRatingKey = getKeyName("restaurants_by_rating")
